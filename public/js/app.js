2d3c6ff3601ad3bb90497ec1a29b4f38ec08b139
santiago gil <abcdfgil2019@gmail.com>
2025-12-04 10:41:27 -0300

    n

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtj9fa0St2IMZgo4jfNPsz_3EMVtioyGU",
-  authDomain: "cajas-secuela.firebaseapp.com",
-  projectId: "cajas-secuela"
+    apiKey: "AIzaSyBtj9fa0St2IMZgo4jfNPsz_3EMVtioyGU",
+    authDomain: "cajas-secuela.firebaseapp.com",
+    projectId: "cajas-secuela",
+    storageBucket: "cajas-secuela.firebasestorage.app",
+    messagingSenderId: "551056516132",
+    appId: "1:551056516132:web:88b9da72dd8dd1a8e7a4b1",
+    measurementId: "G-SZDRGMZS4X"
 };
 
 export const app = initializeApp(firebaseConfig);
