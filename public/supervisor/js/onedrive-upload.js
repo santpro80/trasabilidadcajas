3b03629f7b118198d74b70b7001436136d814604
santiago gil <abcdfgil2019@gmail.com>
2026-01-15 12:17:35 -0300

    refresh token

--- public/supervisor/js/onedrive-upload.js
+++ public/supervisor/js/onedrive-upload.js
@@ -2,7 +2,7 @@
 
 const OD_CONFIG = {
     clientId: "56c7f9c1-d4df-41f8-af09-3c3561ccb35a",
-    initialRefreshToken: "M.C501_BL2.0.U.-CkRjnSeZM2TJZyalt5*YUn3NQV0sJcFNy3AEHPP9f!e*wfU7sDIDmzmYcibhGRYQNxlGTJdFCNZuTJcLttSSinrGUI2Xdg2ZpaaHXswtwlNlbOTYlyNQ*!EL5BbFThx0eq7lpU0nQH1nOGxmebd*PRRFPWxnH62A3UJX1cg4HaKl*u3cTwzy!1K63YX61MQ1HyczAEzNTyvrKFXRpdjTjJMeqsSe8ZLz!ecJyuo9RcWHocc49TeAElALjgu879RSh2k!XRNLFhn!IzRuwK4CAZBSqm98wLowKf9knU88*8GNFOurFYX64D4Z3smQJbjp1M7UTJ6OCsHbGGeGf2C7OSeknQ!jlO8Jxa9MusxTVu1g"
+    initialRefreshToken: "M.C501_SN1.0.U.-Cg9Z7RnxdUmjAR7FwCerm60FRztOndv*VV6rpDhs*DqS8uVR508eUh1r!qIDO4rvW79JHAp0T9ye9VEpmqGjuLoa6dbiIx5cpAMl3pehYDTvsOletxToEvimG97CCvJ4MPOr51!FMfr!v3rk4iJ4sDXp9HHcKXgb3!3O9pPrIZutJj5BpxzehAdvOlZTkGjNcQzPJRg1guaErs7vfBVJewzYzVJgops1ZOdZVeckie8KrFgLZPyOcmNLJ3*iCRl6JIvOqRqSRM8y3zwQmlMXh9G5kc8bTYYOO2Mh9FemhXHwcTrZuBrwbsCxnfB!Eehk58Bwcr*bBW*UjYKW7O9d06ZexBpBtY5*vQ3V7vNIBdk*"
 };
 
 async function getODAccessToken() {
