702bc373fcc546b94a332d65536bd43287c865f6
santiago gil <abcdfgil2019@gmail.com>
2026-01-30 15:40:45 -0300

    refresh

--- public/supervisor/js/lista-items-por-caja.js
+++ public/supervisor/js/lista-items-por-caja.js
@@ -8,7 +8,7 @@ import {
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM completamente cargado y analizado");
-    console.log("ELEMENTO DE REFRE");
+    console.log("ELEMENTO DE refresh 2.0 a ver si ahora si");
     const userDisplayNameElement = document.getElementById('user-display-name');
     const logoutBtn = document.getElementById('logout-btn');
     const backBtn = document.getElementById('back-btn');
