aee0207b9852ee9e7de4b11def1a6f5230b156d0
santiago gil <abcdfgil2019@gmail.com>
2026-02-03 15:11:49 -0300

    operario 7

--- public/supervisor/js/lista-items-por-caja.js
+++ public/supervisor/js/lista-items-por-caja.js
@@ -3,7 +3,7 @@ import {
     doc, getDoc, setDoc, updateDoc, deleteField, onSnapshot,
     registrarHistorial, appCheck, showNotification,
     registrarMovimientoCaja, sanitizeFieldName, unSanitizeFieldName, registrarConsumoItem,
-    collection, query, where, getDocs
+    collection, query, where, getDocs, uploadToOneDrive
 } from './firebase-config.js';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -412,7 +412,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (searchInput) searchInput.addEventListener('input', () => renderFilteredItems(allLoadedItemsData, searchInput.value));
     if (addItemBtn) addItemBtn.addEventListener('click', addNewItemRow);
-    console.log("debuddedante for pr que estamos de verdad ")
     if (downloadPdfBtn) downloadPdfBtn.addEventListener('click', () => { 
         console.log("Botón de reporte presionado, mostrando modal de tipo de reporte.");
         if (tipoReporteModal) tipoReporteModal.style.display = 'flex'; 
